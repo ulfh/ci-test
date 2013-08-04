2b5ef55aca6d0aea8fa8ece7c953542303222b97
Ulf Holmstedt <ulfh@air.local>
2013-08-04 11:18:18 +0200

    Initial Commit

---
+++ covtest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  covtest
+//
+//  Created by Ulf Holmstedt on 2013-08-04.
+//  Copyright (c) 2013 Ulf Holmstedt. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
